608d2c7f6e905cb34ff2437fd4d8708e4214a9d3
Daniel Müller <acct.d-mueller@mailbox.org>
2022-08-15 20:02:15 +1200

    changes for pylint

--- dev_checks.sh
+++ dev_checks.sh
@@ -3,7 +3,7 @@ isort .
 echo "\n=== BLACK ============\n"
 black .
 echo "\n=== PYLINT ===========\n"
-pylint $(git ls-files '*.py')
+pylint ./**/*.py
 echo "\n=== MYPY =============\n"
 mypy . --strict --ignore-missing-imports
 echo "\n=== PYTEST ===========\n"
