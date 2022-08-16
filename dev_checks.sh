2c398f50daa42a8b088a921f318a48d417f42757
Daniel Müller <acct.d-mueller@mailbox.org>
2022-08-16 16:08:31 +1200

    pyproject.toml & poetry

--- dev_checks.sh
+++ dev_checks.sh
@@ -1,10 +1,23 @@
-echo "\n=== ISORT ============\n"
-isort .
-echo "\n=== BLACK ============\n"
-black .
-echo "\n=== PYLINT ===========\n"
-pylint ./**/*.py
-echo "\n=== MYPY =============\n"
-mypy . --strict --ignore-missing-imports
-echo "\n=== PYTEST ===========\n"
-pytest --cov
+COLOR='\033[1;37m'
+NOCOLOR='\033[0m'
+
+echo "$COLOR\n=== ISORT on src ============\n$NOCOLOR"
+poetry run isort src
+echo "$COLOR\n=== ISORT on tests ==========\n$NOCOLOR"
+poetry run isort tests
+
+echo "$COLOR\n=== BLACK on src ============\n$NOCOLOR"
+poetry run black src
+echo "$COLOR\n=== BLACK on tests ==========\n$NOCOLOR"
+poetry run black tests
+
+echo "$COLOR\n=== PYLINT on src ===========\n$NOCOLOR"
+poetry run pylint src
+echo "$COLOR\n=== PYLINT on tests =========\n$NOCOLOR"
+poetry run pylint tests
+
+echo "$COLOR\n=== MYPY ====================\n$NOCOLOR"
+poetry run mypy .
+
+echo "$COLOR\n=== PYTEST ==================\n$NOCOLOR"
+poetry run pytest .
